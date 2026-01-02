fde3218e7897de31590f80447edc6b8fc6b9ed7b
Yann <227698+yanncarlier@users.noreply.github.com>
2026-01-02 09:46:42 +0700

    fixed some issues

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
 }
 
